ffe254e52abdafc80d250220bc4a51c1da52432f
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-30 00:36:21 -0500

    Fix camera going off bounds

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -2,18 +2,24 @@ namespace SideScrollGame;
 
 public partial class CameraController : Camera2D
 {
-    private float ScrollSpeed { get; } = 15;
+    private float ScrollSpeed { get; } = 8;
 
     public override void _PhysicsProcess(double delta)
     {
+        var cameraWidth = GetViewportRect().Size.X / Zoom.X;
+
         if (Input.IsActionPressed("move_left"))
         {
-            Position -= new Vector2(ScrollSpeed, 0);
+            // Prevent the camera from going too far left
+            if (Position.X - (cameraWidth / 2) > LimitLeft)
+                Position -= new Vector2(ScrollSpeed, 0);
         }
 
         if (Input.IsActionPressed("move_right"))
         {
-            Position += new Vector2(ScrollSpeed, 0);
+            // Prevent the camera from going too far right
+            if (Position.X + (cameraWidth / 2) < LimitRight)
+                Position += new Vector2(ScrollSpeed, 0);
         }
     }
 }
