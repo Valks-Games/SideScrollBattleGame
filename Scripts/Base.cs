e8df61b7930bb609682c3b4263c26446d4285733
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-03-31 13:44:01 -0500

    Animate base damage

--- Scripts/Base.cs
+++ Scripts/Base.cs
@@ -18,15 +18,23 @@ public partial class Base : Sprite2D, IDamageable
                 return;
             }
 
+            SetPhysicsProcess(true);
+            AnimateTimer.StartMs(AnimateTime);
             LabelCurHealth.Text = value + "";
         }
     }
 
-    private Label LabelMaxHealth { get; set; }
-    private Label LabelCurHealth { get; set; }
+    private GTimer AnimateTimer      { get; set; }
+    private Label  LabelMaxHealth    { get; set; }
+    private Label  LabelCurHealth    { get; set; }
+    private int    AnimateTime       { get; } = 300;
+    private int    AnimateAmplitudeX { get; } = 1;
+    private int    AnimateAmplitudeY { get; } = 1;
 
     public override void _Ready()
     {
+        SetPhysicsProcess(false);
+        AnimateTimer = new GTimer(this, () => SetPhysicsProcess(false), AnimateTime);
         AddToGroup(Team.ToString());
 
         var hbox = GetNode<HBoxContainer>("HBox");
@@ -35,6 +43,14 @@ public partial class Base : Sprite2D, IDamageable
         LabelCurHealth = hbox.GetNode<Label>("CurHealth");
 
         LabelMaxHealth.Text = MaxHealth + "";
-        CurHealth = MaxHealth;
+        LabelCurHealth.Text = MaxHealth + "";
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var randX = (float)GD.RandRange(-1d, 1d);
+        var randY = (float)GD.RandRange(-1d, 1d);
+
+        Offset = new Vector2(randX * AnimateAmplitudeX, randY * AnimateAmplitudeY);
     }
 }
