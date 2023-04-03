66ca8a56fb2c0b0ee3a115e0267be2eeffdefa5a
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-03 00:14:18 -0500

    Move roll attack code to Attacks.cs

---
+++ Scripts/Attacks.cs
@@ -0,0 +1,51 @@
+﻿namespace SideScrollGame;
+
+public static class Attacks
+{
+    /// <summary>
+    /// Roll while staying still, then roll super fast forward
+    /// </summary>
+    public static void Spinning(Entity entity, Action callback) 
+    {
+        var tween = entity.AttackTween;
+
+        tween = entity.CreateTween();
+        var rot = Mathf.Pi * 2;
+
+        tween.TweenProperty(entity.AnimatedSprite, "rotation", rot, 0.5);
+
+        rot += Mathf.Pi * 2 * 9;
+
+        tween.TweenProperty(entity.AnimatedSprite, "rotation", rot, 0.5);
+        tween.Parallel().TweenProperty(
+            entity.AnimatedSprite, "position:x", entity.DetectionRange, 0.5);
+
+        tween.TweenCallback(Callable.From(() =>
+        {
+            entity.Attack();
+        }));
+
+        tween.TweenProperty(entity.AnimatedSprite, 
+            "position", new Vector2(9, -2), 0.1);
+        tween.TweenProperty(entity.AnimatedSprite, 
+            "position", new Vector2(8, 0), 0.3);
+
+        rot -= Mathf.Pi * 2 * 2;
+
+        tween.TweenProperty(entity.AnimatedSprite, "rotation", rot, 0.8)
+            .SetDelay(0.5);
+        tween.Parallel().TweenProperty(entity.AnimatedSprite, 
+            "position", new Vector2(0, 0), 0.8)
+            .SetDelay(0.5);
+
+        tween.TweenCallback(Callable.From(() =>
+        {
+            // This entity was destroyed (is there a better way of doing this?)
+            if (!GodotObject.IsInstanceValid(entity))
+                return;
+
+            entity.AnimatedSprite.Rotation = 0;
+            callback();
+        }));
+    }
+}
--- Scripts/StateAttack.cs
+++ Scripts/StateAttack.cs
@@ -6,40 +6,7 @@ public class StateAttack : State<Entity>
 
     public override void EnterState()
     {
-        Entity.AttackTween = Entity.CreateTween();
-        var rot = Mathf.Pi * 2;
-
-        Entity.AttackTween.TweenProperty(Entity.AnimatedSprite, "rotation", rot, 0.5);
-
-        rot += Mathf.Pi * 2 * 9;
-
-        Entity.AttackTween.TweenProperty(Entity.AnimatedSprite, "rotation", rot, 0.5);
-        Entity.AttackTween.Parallel().TweenProperty(Entity.AnimatedSprite, "position:x", Entity.DetectionRange, 0.5);
-
-        Entity.AttackTween.TweenCallback(Callable.From(() =>
-        {
-            Entity.Attack();
-        }));
-
-        Entity.AttackTween.TweenProperty(Entity.AnimatedSprite, "position", new Vector2(9, -2), 0.1);
-        Entity.AttackTween.TweenProperty(Entity.AnimatedSprite, "position", new Vector2(8, 0), 0.3);
-        
-        rot -= Mathf.Pi * 2 * 2;
-
-        Entity.AttackTween.TweenProperty(Entity.AnimatedSprite, "rotation", rot, 0.8)
-            .SetDelay(0.5);
-        Entity.AttackTween.Parallel().TweenProperty(Entity.AnimatedSprite, "position", new Vector2(0, 0), 0.8)
-            .SetDelay(0.5);
-
-        Entity.AttackTween.TweenCallback(Callable.From(() =>
-        {
-            // This entity was destroyed (is there a better way of doing this?)
-            if (!GodotObject.IsInstanceValid(Entity))
-                return;
-
-            Entity.AnimatedSprite.Rotation = 0;
-            SwitchState(StateType.Cooldown);
-        }));
+        Attacks.Spinning(Entity, () => SwitchState(StateType.Cooldown));
     }
 
     public override void Update()
