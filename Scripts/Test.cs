88397a285d1b29f78fcee595fa0261ca89837483
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-05 03:22:26 -0500

    Update work

--- Scripts/Test.cs
+++ Scripts/Test.cs
@@ -1,22 +1,21 @@
-using static Godot.Animation;
-using static Godot.Tween;
-
 namespace SideScrollGame;
 
-public partial class Test : Node
+public partial class Test : Node2D
 {
     private GPath Path { get; set; }
 
     public override void _Ready()
     {
         var points = GetNode("Levels").GetChildren<Node2D>().Select(x => x.Position).ToArray();
-        Path = new GPath(this, points);
+        Path = new GPath(points, Colors.White);
         Path.AddCurves();
 
         var sprite = GetNode<Sprite2D>("Sprite2D");
         RemoveChild(sprite);
 
         Path.AddSprite(sprite);
+
+        AddChild(Path);
     }
 
     public override void _Input(InputEvent @event)
@@ -24,10 +23,10 @@ public partial class Test : Node
         if (@event is InputEventKey inputEventKey)
         {
             if (inputEventKey.IsKeyJustPressed(Key.D))
-                Path.AnimateNext();
+                Path.AnimateForwards();
 
             if (inputEventKey.IsKeyJustPressed(Key.A))
-                Path.AnimatePrev();
+                Path.AnimateBackwards();
         }
     }
 }
