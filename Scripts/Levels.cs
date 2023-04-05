032b2cb1e38b775cb7fd8780f5ece6a357215b96
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-04 20:39:20 -0500

    More bezier curve map code

--- Scripts/Levels.cs
+++ Scripts/Levels.cs
@@ -2,90 +2,84 @@ namespace SideScrollGame;
 
 public partial class Levels : Node2D
 {
-    private float V1 { get; set; }
-    private float V2 { get; set; }
-    private float V3 { get; set; }
-    private float V4 { get; set; }
+    private Path2D Path2D { get; set; }
+    private PathFollow2D PathFollow2D { get; set; }
+    private float[] TValues { get; set; }
+    private int TIndex { get; set; }
+    private Vector2[] LevelIconPositions { get; set; }
+    private Tween Tween { get; set; }
+    private Tween.TransitionType TransType { get; } = Tween.TransitionType.Sine;
+    private Tween.EaseType EaseType { get; } = Tween.EaseType.Out;
+    private double Duration { get; } = 1.5;
 
     public override void _Ready()
     {
-        var canvasLayer = GetNode<CanvasLayer>("../CanvasLayer");
+        Path2D = GetNode<Path2D>("../Path2D");
+        PathFollow2D = Path2D.GetNode<PathFollow2D>("PathFollow2D");
 
-        var vbox = new VBoxContainer();
-        vbox.AddThemeConstantOverride("separation", 0);
-        canvasLayer.AddChild(vbox);
-
-        var slider1 = new UISlider(new SliderOptions
+        var slider = new UISlider(new SliderOptions
         {
-            Name = "Debug",
-            HSlider = new HSlider {
-                MinValue = -1000,
-                MaxValue = 1000    
-            },
-            HideBackPanel = false
-        });
-        slider1.ValueChanged += v => V1 = v;
-
-        var slider2 = new UISlider(new SliderOptions
-        {
-            Name = "Debug",
             HSlider = new HSlider
             {
-                MinValue = -1000,
-                MaxValue = 1000
-            },
-            HideBackPanel = false
+                Step = 0.01,
+                MaxValue = 1
+            }
         });
-        slider2.ValueChanged += v => V2 = v;
+        slider.ValueChanged += v => PathFollow2D.ProgressRatio = v;
 
-        var slider3 = new UISlider(new SliderOptions
-        {
-            Name = "Debug",
-            HSlider = new HSlider
-            {
-                MinValue = -1000,
-                MaxValue = 1000
-            },
-            HideBackPanel = false
-        });
-        slider3.ValueChanged += v => V3 = v;
+        GetNode<CanvasLayer>("../CanvasLayer").AddChild(slider);
 
-        var slider4 = new UISlider(new SliderOptions
-        {
-            Name = "Debug",
-            HSlider = new HSlider
-            {
-                MinValue = -1000,
-                MaxValue = 1000
-            },
-            HideBackPanel = false
-        });
-        slider4.ValueChanged += v => V4 = v;
+        LevelIconPositions = this.GetChildren<Node2D>().Select(x => x.Position).ToArray();
 
-        vbox.AddChild(slider1);
-        vbox.AddChild(slider2);
-        vbox.AddChild(slider3);
-        vbox.AddChild(slider4);
+        var count = LevelIconPositions.Count();
+
+        TValues = new float[count];
+
+        for (int i = 0; i < count; i++)
+            TValues[i] = (float)i / (count - 1);
     }
 
-    public override void _Process(double delta)
+    public override void _Input(InputEvent @event)
     {
-        QueueRedraw();
+        if (@event is InputEventKey inputEventKey)
+        {
+            if (inputEventKey.IsKeyJustPressed(Key.D))
+            {
+                TIndex = Mathf.Min(TIndex + 1, TValues.Count() - 1);
+
+                Tween?.Kill();
+                Tween = PathFollow2D.CreateTween();
+                Tween.TweenProperty(PathFollow2D, "progress_ratio", TValues[TIndex], Duration)
+                    .SetTrans(TransType)
+                    .SetEase(EaseType);
+            }
+
+            if (inputEventKey.IsKeyJustPressed(Key.A))
+            {
+                TIndex = Mathf.Max(TIndex - 1, 0);
+
+                Tween?.Kill();
+                Tween = PathFollow2D.CreateTween();
+                Tween.TweenProperty(PathFollow2D, "progress_ratio", TValues[TIndex], Duration)
+                    .SetTrans(TransType)
+                    .SetEase(EaseType);
+            }
+        }
     }
 
     public override void _Draw()
     {
-        var points = this.GetChildren<Node2D>().Select(x => x.Position).ToArray();
-
-        for (int i = 0; i < points.Count() - 1; i++)
+        for (int i = 0; i < LevelIconPositions.Count() - 1; i++)
         {
-            var pointA = points[i];
-            var pointB = points[i + 1];
+            var pointA = LevelIconPositions[i];
+            var pointB = LevelIconPositions[i + 1];
 
-            var c1 = new Vector2(-299 + V1, 102 + V2);
-            var c2 = new Vector2( 243 + V3, 198 + V4);
+            var c1 = new Vector2(-299, 102);
+            var c2 = new Vector2( 243, 198);
 
-            BezierCurve.Draw(this, pointA, pointB, c1, c2, Colors.White);
+            var points = BezierCurve.Draw(this, pointA, pointB, c1, c2, Colors.White);
+            foreach (var point in points)
+                Path2D.Curve.AddPoint(point);
         }
     }
 }
