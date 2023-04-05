1144189f7453295ca82cd7c0c8147a7f2c5a6083
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-05 01:28:14 -0500

    Cleanup code

--- Scripts/Levels.cs
+++ Scripts/Levels.cs
@@ -4,7 +4,7 @@ public partial class Levels : Node2D
 {
     private Path2D Path2D { get; set; }
     private PathFollow2D PathFollow2D { get; set; }
-    private float[] TValues { get; set; }
+    private float[] TweenValues { get; set; }
     private int TIndex { get; set; }
     private Vector2[] LevelIconPositions { get; set; }
     private Tween Tween { get; set; }
@@ -23,7 +23,7 @@ public partial class Levels : Node2D
 
         var count = LevelIconPositions.Count();
 
-        TValues = new float[count];
+        TweenValues = new float[count];
 
         // Add a point for each level icon position
         for (int i = 0; i < count; i++)
@@ -38,45 +38,44 @@ public partial class Levels : Node2D
             var B = LevelIconPositions[i + 1];
 
             var center = (A + B) / 2;
-            var offset = ((B - A).Orthogonal().Normalized() * 50 * invert);
+            var curveDistance = 50; // How far the curve is pushed out
+            var offset = ((B - A).Orthogonal().Normalized() * curveDistance * invert);
             var newPos = center + offset;
 
+            // Switch between sides so curves flow more naturally
             invert *= -1;
 
+            Vector4 v;
+
+            // These values were found through trial and error
+            // If you see a simpler pattern than this, please tell me lol
             if (B.Y >= A.Y)
-            {
                 if (B.X >= A.X)
-                {
-                    Path2D.Curve.AddPoint(newPos,
-                        new Vector2(-50, -50), new Vector2(50, 50), 1 + i * 2);
-                }
+                    // Next point is under and after first point
+                    v = new Vector4(-1, -1, 1, 1);
                 else
-                {
-                    Path2D.Curve.AddPoint(newPos,
-                        new Vector2(50, -50), new Vector2(-50, 50), 1 + i * 2);
-                }
-            }
+                    // Next point is under and before first point
+                    v = new Vector4(1, -1, -1, 1);
             else
-            {
                 if (B.X <= A.X)
-                {
-                    Path2D.Curve.AddPoint(newPos,
-                        new Vector2(50, 50), new Vector2(-50, -50), 1 + i * 2);
-                }
+                    // Next point is over and before first point
+                    v = new Vector4(1, 1, -1, -1);
                 else
-                {
-                    Path2D.Curve.AddPoint(newPos,
-                        new Vector2(-50, 50), new Vector2(50, -50), 1 + i * 2);
-                } 
-            }
-                
+                    // Next point is over and after first point
+                    v = new Vector4(-1, 1, 1, -1);
+
+            var curveSize = 50; // How big the curve is
+            var index = 1 + i * 2;
+
+            // Insert the curved point at the index in the curve
+            Path2D.Curve.AddPoint(newPos, 
+                new Vector2(v.X, v.Y) * curveSize, 
+                new Vector2(v.Z, v.W) * curveSize, index);
         }
 
         // Calculate all the progress values for Tween animations
         for (int i = 0; i < count; i++)
-        {
-            TValues[i] = Path2D.Curve.GetClosestOffset(LevelIconPositions[i]);
-        }
+            TweenValues[i] = Path2D.Curve.GetClosestOffset(LevelIconPositions[i]);
     }
 
     public override void _Input(InputEvent @event)
@@ -85,11 +84,11 @@ public partial class Levels : Node2D
         {
             if (inputEventKey.IsKeyJustPressed(Key.D))
             {
-                TIndex = Mathf.Min(TIndex + 1, TValues.Count() - 1);
+                TIndex = Mathf.Min(TIndex + 1, TweenValues.Count() - 1);
 
                 Tween?.Kill();
                 Tween = PathFollow2D.CreateTween();
-                Tween.TweenProperty(PathFollow2D, "progress", TValues[TIndex], Duration)
+                Tween.TweenProperty(PathFollow2D, "progress", TweenValues[TIndex], Duration)
                     .SetTrans(TransType)
                     .SetEase(EaseType);
             }
@@ -100,7 +99,7 @@ public partial class Levels : Node2D
 
                 Tween?.Kill();
                 Tween = PathFollow2D.CreateTween();
-                Tween.TweenProperty(PathFollow2D, "progress", TValues[TIndex], Duration)
+                Tween.TweenProperty(PathFollow2D, "progress", TweenValues[TIndex], Duration)
                     .SetTrans(TransType)
                     .SetEase(EaseType);
             }
